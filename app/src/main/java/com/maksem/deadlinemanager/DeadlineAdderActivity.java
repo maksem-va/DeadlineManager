b827a031f86f21071256d2085366719dbeefee0b
maksem.va <runk147@yandex.ru>
2019-10-30 19:54:18 +0300

    0.0.9.3 Now you can create your deadlines (just one at this version) Unnecessary things removed Graddle setting upgraded

---
+++ app/src/main/java/com/maksem/deadlinemanager/DeadlineAdderActivity.java
@@ -0,0 +1,41 @@
+package com.maksem.deadlinemanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DeadlineAdderActivity extends AppCompatActivity implements OnClickListener {
+
+    EditText eventName;
+    EditText description;
+
+    Button btnCreate;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deadline_adder);
+
+        eventName = findViewById(R.id.eventName);
+        description = findViewById(R.id.description);
+
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("eventName", eventName.getText().toString());
+        intent.putExtra("description", description.getText().toString());
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/maksem/deadlinemanager/MainActivity.java
+++ app/src/main/java/com/maksem/deadlinemanager/MainActivity.java
@@ -1,8 +1,10 @@
 package com.maksem.deadlinemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.EventLogTags;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,22 +15,42 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    TextView event;
+    TextView eventDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        event = findViewById(R.id.event);
+        eventDescription = findViewById(R.id.description);
+
+        Intent intent = getIntent();
+
+        String eventName = intent.getStringExtra("eventName");
+        String description = intent.getStringExtra("description");
+
+        event.setText("Event: " + eventName);
+        eventDescription.setText("Description:" + description);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "You can't add a new deadline at this version of application", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "You can't add a new deadline at this version of application", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                goToTaskListActivity(view);
+            }
+            private void goToTaskListActivity(View view) {
+                Intent intent = new Intent(view.getContext(), DeadlineAdderActivity.class);
+                startActivity(intent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
